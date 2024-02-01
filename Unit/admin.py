4dd809f49c6238eb37f625cf5c51d96a5c0e29d7
EmmanuelKusiluka <emmanuelkusiluka@gmail.com>
2024-02-02 02:52:08 +0300

    more admin registerd models

--- Unit/admin.py
+++ Unit/admin.py
@@ -10,3 +10,12 @@ class StudentAdmin(admin.ModelAdmin):
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['name']
+
+@admin.register(Module)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['username']
