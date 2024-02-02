dd91fdbe847593cf2e2fd00f14410e8e08988b09
EmmanuelKusiluka <emmanuelkusiluka@gmail.com>
2024-02-02 04:15:13 +0300

    pass

--- Unit/forms.py
+++ Unit/forms.py
@@ -3,19 +3,23 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Student, Teacher
 
 class StudentRegistrationForm(UserCreationForm):
-    class Meta:
-        model = Student
-        fields = ['username','firt_name', 'middle_name', 'last_name', 'enrolled_course', 'password1', 'password2']
+    pass
+#     class Meta:
+#         model = Student
+#         fields = ['username', 'firt_name', 'middle_name', 'last_name', 'enrolled_course', 'password1', 'password2']
 
 class StudentAuthenticationForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    pass
+#     username = forms.CharField()
+#     password = forms.CharField(widget=forms.PasswordInput)
 
 class TeacherRegistrationForm(UserCreationForm):
-    class Meta:
-        model = Teacher
-        fields = ['username', 'password1', 'password2']
+    pass
+#     class Meta:
+#         model = Teacher
+#         fields = ['username', 'password1', 'password2']
 
 class TeacherAuthenticationForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    pass
+#     username = forms.CharField()
+#     password = forms.CharField(widget=forms.PasswordInput)
