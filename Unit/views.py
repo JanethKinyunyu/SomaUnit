65b5caed0626f27477c2f6a70015963199980597
JanethKinyunyu <kinyunyujaneth21@gmail.com>
2024-02-02 04:57:02 +0300

    ..

--- Unit/views.py
+++ Unit/views.py
@@ -58,7 +58,7 @@ def home(request):
 
 
 def sign_up(request):
-    return render(request, 'signup.html')
+    return render(request, 'SomaUnit/signup.html')
 
 def StudentDash(request):
     return render(request, 'SomaUnit/dashboards/Student_dashboard.html')
